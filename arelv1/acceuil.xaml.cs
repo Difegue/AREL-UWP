674e47ab1ce1164d47ed8f6d4b4a1e91433ffabd
Tjiho Lun <tom.darboux@eisti.fr>
2016-04-14 16:38:01 +0200

    [AJOUT]:burgermenu - utilisation couleur systeme
    ==ACTION==
    -coloration du fond du menu avec la couleur systeme de l'utilisateur

--- arelv1/acceuil.xaml.cs
+++ arelv1/acceuil.xaml.cs
@@ -24,13 +24,12 @@ namespace arelv1
         {
             InitializeComponent();
             ecrire(getinfo("api/events"));
-            //ecrire(localSettings.Values["token"].ToString());
         }
 
 
         private string getinfo(string url)
         {
-            url = "https://arel.eisti.fr/api/campus/sites";
+            url = "https://arel.eisti.fr/api/campus/rooms";
             string contentType = "application/xml";
             string identifiants = localSettings.Values["token"].ToString();
             string data = "format=xml";
@@ -38,84 +37,6 @@ namespace arelv1
             string resultat = page.http(url, contentType, identifiants,"Bearer", data);//on fait la requete
             return resultat;
         }
-        /*
-        private void requete(IAsyncResult asynchronousResult)
-        {
-
-
-            HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
-
-            // End the operation
-            Stream postStream = request.EndGetRequestStream(asynchronousResult);
-
-
-            //string postData = "grant_type=client_credentials&scope=read&format=xml";
-            string postData = "format=xml";
-            // Convert the string into a byte array.
-            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-
-            // Write to the request stream.
-            postStream.Write(byteArray, 0, postData.Length);
-
-            // Start the asynchronous operation to get the response
-            request.BeginGetResponse(new AsyncCallback(reponse), request);
-        }
-
-        //recupère le résultat de la requete
-        private void reponse(IAsyncResult asynchronousResult)
-        {
-
-
-            HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
-
-            // End the operation
-            try
-            {
-
-
-                HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(asynchronousResult);
-                Stream streamResponse = response.GetResponseStream();
-                StreamReader streamRead = new StreamReader(streamResponse);
-                string responseString = streamRead.ReadToEnd();
-
-
-                resultat = responseString;
-            }
-            catch (Exception e)
-            {
-                string erreur = e.ToString();
-                if (erreur.IndexOf("400") > -1)
-                {
-                    resultat = "identifiants incorrects";
-                }
-                else if (erreur.IndexOf("401") > -1)
-                {
-                    resultat = "acces refusé";
-                }
-                else if (erreur.IndexOf("être résolu") > -1)
-                {
-                    resultat = "Pas de connection internet";
-                }
-                else if(erreur.IndexOf("403") > -1)
-                {
-                    resultat = "ressource interdite";
-                }
-                else
-                {
-                    resultat = "erreur inconnu..."+e;
-                }
-                //contexte = new Contexte { Valeur = "error: " + erreur };
-
-            }
-            allDone.Set();
-        }
-        */
-
-
-
-
-
-
 
         
 
